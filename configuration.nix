aef307c247a19c56524fed2a6de074b5050f2feb
Loïc Daudé Mondet <loic.daude@mondet.fr>
2026-02-06 04:15:43 +0100

    update

--- configuration.nix
+++ configuration.nix
@@ -45,6 +45,10 @@ in
       "vt.default_red=30,243,166,249,137,245,148,186,88,243,166,249,137,245,148,166"
       "vt.default_grn=30,139,227,226,180,194,226,194,91,139,227,226,180,194,226,173"
       "vt.default_blu=46,168,161,175,250,231,213,222,112,168,161,175,250,231,213,200"
+      "zswap.enabled=1" # enables zswap
+      "zswap.compressor=zstd" # compression algorithm
+      "zswap.max_pool_percent=20" # maximum percentage of RAM that zswap is allowed to use
+      "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
     ];
 
     initrd = {
@@ -181,9 +185,10 @@ in
       nil
       nixd
       obs-studio
-      (kdePackages.kdenlive.override {
-        ffmpeg-full = pkgs.ffmpeg_7-full;
-      })
+      # (kdePackages.kdenlive.override {
+      #   ffmpeg-full = pkgs.ffmpeg_7-full;
+      # })
+      kdePackages.kdenlive
       onlyoffice-desktopeditors
     ];
   };
