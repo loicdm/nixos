32caf9cb16f4cd49b323bdf0f4a27979993dd68e
Loïc Daudé Mondet <loic.daude@mondet.fr>
2026-04-30 00:04:12 +0200

    clean up flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "NixOS config – loicdm-pc";
 
   inputs = {
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
@@ -13,33 +12,45 @@
 
   outputs =
     {
-      self,
       nixpkgs,
       home-manager,
       ...
     }:
     let
       system = "x86_64-linux";
+
+      mkSystem =
+        hostname:
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+
+          modules = [
+            # Base config
+            ./configuration.nix
+
+            # Allow unfree packages
+            { nixpkgs.config.allowUnfree = true; }
+
+            # Home Manager
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+
+                users = {
+                  loicdm = import ./home/loicdm.nix;
+                  sasha = import ./home/sasha.nix;
+                  root = import ./home/root.nix;
+                };
+              };
+            }
+          ];
+        };
     in
     {
-      nixosConfigurations.loicdm-pc = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          # Autorise les paquets non-free
-          { nixpkgs.config.allowUnfree = true; }
-
-          ./configuration.nix
-
-          # Home Manager
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.loicdm = import ./home/loicdm.nix;
-            home-manager.users.sasha = import ./home/sasha.nix;
-            home-manager.users.root = import ./home/root.nix;
-          }
-        ];
+      nixosConfigurations = {
+        loicdm-pc = mkSystem "loicdm-pc";
       };
     };
 }
